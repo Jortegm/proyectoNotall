61ac5b1e2a20b31388477617cf7062f853d96f23
jortegm <juanortegamarin@gmail.com>
2017-05-04 13:36:57 +0200

    Diseño y añadido de usuarios a BBBDD a traves de symfony Eliminar usuarios mediante su ID

--- src/Zaide/UsuariosBundle/Controller/UserController.php
+++ src/Zaide/UsuariosBundle/Controller/UserController.php
@@ -2,12 +2,12 @@
 
 namespace Zaide\UsuariosBundle\Controller;
 
-use AppBundle\AppBundle;
 use AppBundle\Entity\Usuarios;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Form\UsuariosType;
-
+use AppBundle\Entity\Post;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller {
 
@@ -28,20 +28,54 @@ class UserController extends Controller {
 
     /** Funcion que añada usuarios a la base de datos.
      * @return Response
+     *
      */
-    public function addUserAction() {
-        $user = new Usuarios();
-        $form = $this->createCreateForm($user);
+    public function addUserAction(Request $request) {
+            $user = new Usuarios();
+            $form = $this->createForm(UsuariosType::class, $user);
+            $form->handleRequest($request);
 
-        return $this->render('UsuariosBundle:User:add.html.twig', array($form->createView()));
+            if ($form->isSubmitted() && $form->isValid()) {
+                $user-> setFechaAlta(new \DateTime("now"));
+                $user ->setFechaActualizacion(new \DateTime("now"));
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('estado', 'Usuario Añadido Correctamente');
+                return $this->redirectToRoute( 'usuarios_registro',['Usuario'=> $user->getId()] );
+            }
+
+
+        return $this->render('UsuariosBundle:User:add.html.twig', ['form' => $form->createView()]);
     }
 
-    private function createCreateForm($user, $entity) {
-        $form = $this->createForm(new UsuariosType(), $entity, array(
-                                    'action'=> $this->generateUrl('usuarios_create'),
-                                    'method'=>'POST')
-                                  );
-        return $form;
+//    public function editAction (Usuarios $usuarios) {
+//        em = $this->getDoctrine()->getManager();
+//        $em->edit($usuarios);
+//        $em->flush();
+//        $this->addFlash('estado', 'Usuario modificado con éxito');
+//
+//
+//        return $this->redirectToRoute('usuarios_registro');
+//
+//    }
+//
+//
+//    }
+
+
+
+
+    public function deleteAction(Usuarios $usuarios) {
+
+        $em = $this->getDoctrine()->getManager();
+            $em->remove($usuarios);
+            $em->flush();
+            $this->addFlash('estado', 'Usuario eliminado con éxito');
+
+
+            return $this->redirectToRoute('usuarios_registro');
+
     }
 
 }
