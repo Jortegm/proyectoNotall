e3f32ea448d0fa7305cd6fd29cc42cc8c553840b
jortegm <juanortegamarin@gmail.com>
2017-05-05 13:29:29 +0200

    Añadido eliminar y editar de  ADMIN de USUARIOS Comienzo de login y Registro

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use AppBundle\Entity\Usuarios;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\UsuariosType;
+use AppBundle\Entity\Post;
+
 
 class DefaultController extends Controller {
     /**
@@ -40,6 +43,36 @@ class DefaultController extends Controller {
         // replace this example code with whatever you need
         return $this->render('para/para.htmil.twig');
     }
+
+    /**
+     * @Route("/registro", name="Registro")
+     */
+    public function registroAction(Request $request){
+        // replace this example code with whatever you need
+        $user = new Usuarios();
+        $form = $this->createForm(UsuariosType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user-> setFechaAlta(new \DateTime("now"));
+            $user ->setFechaActualizacion(new \DateTime("now"));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('estado', 'Ya formas parte de nosotros, comienza modificando tu perfil y a componer ');
+            return $this->redirectToRoute( 'usuario_alta',['Usuario'=> $user->getId()] );
+        }
+        return $this->render('registro/registro.html.twig');
+    }
+
+    /**
+     * @Route("/log", name="Login")
+     */
+    public function loginAction(Request $request){
+        // replace this example code with whatever you need
+        return $this->render('login/login.html.twig');
+    }
+
 
 
 
--- src/Zaide/UsuariosBundle/Controller/UserController.php
+++ src/Zaide/UsuariosBundle/Controller/UserController.php
@@ -15,11 +15,11 @@ use Symfony\Component\HttpFoundation\Request;
 class UserController extends Controller {
 
 
-
     public function principalAdminAction() {
         // replace this example code with whatever you need
         return $this->render('@Usuarios/User/PrincipalAdmin.html.twig');
     }
+
 
     public function indexAction(Request $request) {
         /** @var EntityManager $em */
@@ -31,8 +31,8 @@ class UserController extends Controller {
             ->getResult();
 
         $paginacion = $this->get('knp_paginator');
-        $pagination = $paginacion->paginate($users, $request->query->getInt('page', 1), 3);
-        $users = $em -> getRepository('AppBundle:Usuarios')-> findAll();
+        $pagination = $paginacion->paginate($users, $request->query->getInt('page', 1), 10);
+        //$users = $em -> getRepository('AppBundle:Usuarios')-> findAll();
 
         return $this->render('UsuariosBundle:User:index.html.twig', array('pagination' => $pagination));
 
@@ -44,55 +44,50 @@ class UserController extends Controller {
      */
     public function addUserAction(Request $request) {
             $user = new Usuarios();
-            $form = $this->createForm(UsuariosType::class, $user);
-            $form->handleRequest($request);
+            $forma = $this->createForm(UsuariosType::class, $user);
+            $forma->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
+            if ($forma->isSubmitted() && $forma->isValid()) {
                 $user-> setFechaAlta(new \DateTime("now"));
                 $user ->setFechaActualizacion(new \DateTime("now"));
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($user);
                 $em->flush();
                 $this->addFlash('estado', 'Usuario Añadido Correctamente');
-                return $this->redirectToRoute( 'usuarios_registro',['Usuario'=> $user->getId()] );
+                return $this->redirectToRoute( 'homepage',['Usuario'=> $user->getId()] );
             }
 
 
-        return $this->render('UsuariosBundle:User:add.html.twig', ['form' => $form->createView()]);
+        return $this->render('UsuariosBundle:User:add.html.twig', ['forma' => $forma->createView()]);
     }
 
     public function editAction(Request $request) {
-        /** @var EntityManager $em */
-        $usuario = new Usuarios();
         $em = $this->getDoctrine()->getManager();
 
-        if (null == $usuario) {
-            $categoria = new Categoria();
-            $em->persist($categoria);
-        }
-
-        $form = $this->createForm(CategoriaType::class, $categoria);
-
+        $idUsuario = $request->get('id');
+        $usuario = $em->getRepository('AppBundle:Usuarios')->find($idUsuario);
+        $form = $this->createForm(UsuariosType::class, $usuario);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
             $this->addFlash('estado', 'Cambios guardados con éxito');
-            return $this->redirectToRoute('listadoCategorias',['categoria'=>$categoria->getId()]);
+            return $this->redirectToRoute('usuarios_registro', ['usuarios'=>$usuario->getId()]);
         }
 
-        return $this->render('categorias/form.html.twig', [
-            'categoria' => $categoria,
-            'form' => $form->createView()
+        return $this->render('UsuariosBundle:User:edit.html.twig', ['usuarios' => $usuario, 'form' => $form->createView()
         ]);
     }
 
 
-    public function deleteAction(Usuarios $usuarios) {
+    public function deleteAction(Request $request) {
 
         $em = $this->getDoctrine()->getManager();
-            $em->remove($usuarios);
-            $em->flush();
+        $idUsuario = $request->get('id');
+        $usuario = $em->getRepository('AppBundle:Usuarios')->find($idUsuario);
+        $em->remove($usuario);
+     //   $em->persist($usuario);
+        $em->flush();
             $this->addFlash('estado', 'Usuario eliminado con éxito');
 
 
