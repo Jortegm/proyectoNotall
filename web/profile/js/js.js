756e4f5ad1608dc7492ee2fcfedf317a04bf8d69
jortegm <juanortegamarin@gmail.com>
2017-05-25 13:10:59 +0200

    ultima actualizacion

--- web/profile/js/js.js
+++ web/profile/js/js.js
@@ -1,23 +1,24 @@
 
 $( window ).load(function() {
-    $("#editar").click(function(e){
+    $("#editar").click(function(e) {
         e.preventDefault();
         $("#info").hide();
         $("#modificar").show();
         $("div").eq(43).hide();
         $("div").eq(44).hide();
 
-
-    $("a").click(function () {
-
-        $("#perfil").hide();
-        $("div#option.w").hide()
     });
-
 
     $("a.btn.btn-lg.btn-danger").click(function() {
        $("#option").show();
     });
 
+
+    $("#partitura").click(function () {
+        $("#option1").hide();
     });
+    $("#listado").click(function () {
+        $("#option1").hide();
+    });
+
 });
--- web/profile/js/vex.js
+++ web/profile/js/vex.js
@@ -1,23 +1,86 @@
 
+window.addEventListener("load",function () {
+    //se añade una partituro vacia como plantilla para el usuario
+    partituraVacia();
+    //añadir una nueva partitura:
+    var nex = document.getElementById("new");
+        //añadimos un evento en que consiste en eliminar la pantilla (partitura por defecto) y añadir una nueva en blanco
+        nex.addEventListener("click", function(){
+          var boo = document.getElementById("boo");
+          boo.remove();
+          var bo = document.createElement("div");
+          bo.setAttribute('id', 'boo');
+          var partitura = document.getElementById("escribe");
+          bo.appendChild(partitura);
+          document.getElementById("part").appendChild(bo);
+          partituraVacia();
+    });
+
+    var clave = document.getElementById("bassClefIcon");
+    clave.addEventListener("click", function () {
+        cambiarClave();
+    });
+
+});
+
+
+/**
+ * Function que crea y dibuja una partitura vacia
+ */
+function partituraVacia() {
+
 // crear partitura vacia
-VF = Vex.Flow;
+    VF = Vex.Flow;
 
 // Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById("boo")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    var div = document.getElementById("boo")
+    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Configure the rendering context.
-renderer.resize(500, 500);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    renderer.resize(900, 800);
+    var context = renderer.getContext();
+    context.setFont("Arial", 14, "").setBackgroundFillStyle("#eed");
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(10, 40, 400);
+    var stave = new VF.Stave(10, 40, 800);
 
 // Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+    stave.addClef("treble").addTimeSignature("4/4");
 
 // Connect it to the rendering context and draw!
-stave.setContext(context).draw();
+    stave.setContext(context).draw();
+
+}
 
 
+
+function cambiarClave() {
+
+    var boo = document.getElementById("boo");
+    boo.remove();
+    var bo = document.createElement("div");
+    bo.setAttribute('id', 'boo');
+    var partitura = document.getElementById("escribe");
+    bo.appendChild(partitura);
+    document.getElementById("part").appendChild(bo);
+
+    VF = Vex.Flow;
+
+// Create an SVG renderer and attach it to the DIV element named "boo".
+    var div = document.getElementById("boo")
+    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+// Configure the rendering context.
+    renderer.resize(900, 800);
+    var context = renderer.getContext();
+    context.setFont("Arial", 14, "").setBackgroundFillStyle("#eed");
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+    var stave = new VF.Stave(10, 40, 800);
+
+// Add a clef and time signature.
+    stave.addClef("alto").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
+}
