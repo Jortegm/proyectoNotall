7595d4b7ad237d755c761a245289a6e095a71cf8
jortegm <juanortegamarin@gmail.com>
2017-05-29 18:11:38 +0200

    mejora menu tols

--- web/profile/js/vex.js
+++ web/profile/js/vex.js
@@ -1,11 +1,11 @@
 
 window.addEventListener("load",function () {
-    //se añade una partituro vacia como plantilla para el usuario
+    hideTools();
     partituraVacia();
     //añadir una nueva partitura:
     var nex = document.getElementById("new");
         //añadimos un evento en que consiste en eliminar la pantilla (partitura por defecto) y añadir una nueva en blanco
-        nex.addEventListener("click", function(){
+        nex.addEventListener("click",function() {
           var boo = document.getElementById("boo");
           boo.remove();
           var bo = document.createElement("div");
@@ -22,6 +22,18 @@ window.addEventListener("load",function () {
     });
 
 });
+
+/**
+ * Escondemos todo el menu de anotacion
+ */
+function hideTools () {
+    document.getElementById("option1").style.display="none";
+    document.getElementsByClassName("row")[0].style.display="none";
+    document.getElementById("nShow").style.display="none";
+    document.getElementById("articulation").style.display="none";
+    document.getElementById("ornaments").style.display="none";
+    document.getElementById("dinamic").style.display="none";
+}
 
 
 /**
@@ -52,35 +64,3 @@ function partituraVacia() {
 
 }
 
-
-
-function cambiarClave() {
-
-    var boo = document.getElementById("boo");
-    boo.remove();
-    var bo = document.createElement("div");
-    bo.setAttribute('id', 'boo');
-    var partitura = document.getElementById("escribe");
-    bo.appendChild(partitura);
-    document.getElementById("part").appendChild(bo);
-
-    VF = Vex.Flow;
-
-// Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById("boo")
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
-// Configure the rendering context.
-    renderer.resize(900, 800);
-    var context = renderer.getContext();
-    context.setFont("Arial", 14, "").setBackgroundFillStyle("#eed");
-
-// Create a stave of width 400 at position 10, 40 on the canvas.
-    var stave = new VF.Stave(10, 40, 800);
-
-// Add a clef and time signature.
-    stave.addClef("alto").addTimeSignature("4/4");
-
-// Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-}
