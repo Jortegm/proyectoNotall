6156015ce038b49c00c1b4e0710c1b3bad5a9ebf
jortegm <juanortegamarin@gmail.com>
2017-06-01 17:38:58 +0200

    mejora

--- web/profile/js/vex.js
+++ web/profile/js/vex.js
@@ -29,33 +29,18 @@ window.addEventListener("load",function () {
  * Function que crea y dibuja una partitura vacia
  */
 function partituraVacia() {
-const VF = Vex.Flow;
+  const VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var vf = new VF.Factory({renderer: {selector: 'boo', height: 400}});
-var score = vf.EasyScore();
-var system = vf.System();
+  // Create an SVG renderer and attach it to the DIV element named "boo".
+  var vf = new VF.Factory({renderer: {selector: 'boo'}});
+  var score = vf.EasyScore();
+  var system = vf.System();
 
-system.addStave({
-  voices: [
-    score.voice(
-      score.notes('C#5/q, B4')
-        .concat(score.beam(score.notes('A4/8, E4, C4, D4')))
-    )
-  ]
-}).addClef('treble').addTimeSignature('4/4');
+  system.addStave({
+    voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
+  }).addClef('treble').addTimeSignature('4/4');
 
-system.addStave({
-  voices: [
-    score.voice(
-      score.notes('C#5/q, B4, B4')
-        .concat(
-        score.tuplet(score.beam(score.notes('A4/8, E4, C4'))))
-    )
-  ]
-}).addClef('treble').addTimeSignature('2/4');
-
-vf.draw();
+  vf.draw();
 }
 
 
